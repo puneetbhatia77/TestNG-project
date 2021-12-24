ca2cb1313e858d4a83b1f3b07899144e0461f09b
puneetbhatia77 <puneetbhatia77@gmail.com>
2021-12-24 17:51:20 +0530

    Update TestNGClass.java

--- src/mypackage/TestNGClass.java
+++ src/mypackage/TestNGClass.java
@@ -6,26 +6,33 @@ import org.testng.annotations.Test;
 
 public class TestNGClass {
    
-		@AfterTest  
-		   public  void test3()                     // Third test case.  
-		  {  
-		   System.out.println("after test2");  
-		 }
-			  
-		@BeforeTest  
-		    public void test1()                      // First test case.  
-		  {  
-		    System.out.println("before test2");   
-		  }   
-		   
-		  @Test  
-		   public  void test2()                     // Second test case.  
-		  {  
-		   System.out.println("test2-part1");  
-		 }	
-		  @Test
-		   public  void test4()                     // Second test case.  
-			  {  
-			   System.out.println("test2-part2");  
-			 }
+  @Test (priority=2)
+  public void test2() {
+	  System.out.println("Running test 2");
+  }
+  
+  @Test (priority=4)
+  public void test4() {
+	  System.out.println("Running test 4");
+  }
+  @Test (priority=1)
+  public void test1() {
+	  System.out.println("Running test 1");
+  }
+  @Test (priority=3)
+  public void test3() {
+	  System.out.println("Running test 3");
+  }
+  
+  
+  @BeforeTest
+  public void beforeTest() {
+	  System.out.println("Before Running test");
+  }
+
+  @AfterTest
+  public void afterTest() {
+	  System.out.println("After Running test");
+  }
+
 }
